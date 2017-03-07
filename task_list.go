32f145709a6524651c39bb65dd0945c0f7357141
Yeni <yenicapote08@gmail.com>
2017-03-07 08:58:37 -0800

    Added db functionality.

--- task_list.go
+++ task_list.go
@@ -1,25 +1,74 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //TodoItem struct
 type TodoItem struct {
-	ID         int
-	Caption    string
-	IsFinished bool
+	ID      int
+	Caption string
+	Done    bool
 }
 
-//TodoItemsSlice todo object
-var TodoItemsSlice = []TodoItem{}
+//todo
+var todo []TodoItem
 
+//assign global db variable
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("mysql", "root@tcp(a9d7d3790003711e790670a9b77d070e-319954018.us-west-2.elb.amazonaws.com:3306)/list")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	// defer db.Close()
+}
+
+//output
 func main() {
 	fmt.Println("initializing...")
-	TodoItemsSlice = make([]TodoItem, 0)
+	todo = make([]TodoItem, 0)
+
+	rows, err := db.Query("SELECT * FROM todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var task = TodoItem{}
+		var done int
+		err := rows.Scan(&task.ID, &task.Caption, &done)
+		if done == 0 {
+			task.Done = false
+		} else {
+			task.Done = true
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		todo = append(todo, task)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	for _, task := range todo {
+		log.Println("here's caption:", task.Caption)
+	}
+
 	runServer()
 }
 
@@ -30,17 +79,24 @@ func runServer() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	captionFormValue := r.PostFormValue("caption")
-	if captionFormValue != "" {
-		fmt.Println("caption:", captionFormValue)
-		newID := len(TodoItemsSlice) + 1
-		p := &TodoItem{ID: newID, Caption: captionFormValue, IsFinished: false}
-		TodoItemsSlice = append(TodoItemsSlice, *p)
+	newItem := r.URL.Query().Get("caption")
+	if newItem != "" {
+		fmt.Println("caption:", newItem)
+		newTask := TodoItem{Caption: newItem, Done: false}
+
+		//add newItem todo into DB
+		_, err := db.Exec("INSERT INTO todos (ID, Caption, Done) VALUES(?,?,?)", 0, newItem, 0)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		log.Println("caption:", newItem, "added")
+		todo = append(todo, newTask)
 	}
 
 	t, err := template.ParseFiles("/templates/index.html")
 	if err != nil {
-		log.Fatal("can not parse templates/index.html: " + err.Error())
+		log.Fatal("can not parse /templates/index.html: " + err.Error())
 	}
-	t.Execute(w, TodoItemsSlice)
+	t.Execute(w, todo)
 }
